ae238690caa2b40202967a4c696ffe360c09d625
ayan <ayansome1@gmail.com>
2017-12-31 00:01:56 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import GoogleLogin from 'react-google-login'
 import './App.css'
 
 class App extends Component {
-
-  render() {
+  render () {
     const responseGoogle = (response) => {
       console.log(response)
     }
     return (
-      /* <div className='App'>
-         My apps
-       </div>*/
       <GoogleLogin
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Login"
+        clientId='658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
+        buttonText='Login'
         onSuccess={this.responseGoogle}
         onFailure={this.responseGoogle}
       />
