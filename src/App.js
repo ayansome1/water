859fa33a80c21b3133f4f689a0424609801d0739
ayan <ayansome1@gmail.com>
2017-12-31 00:10:12 +0530

    code format

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
       <GoogleLogin
         clientId='658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
         buttonText='Login'
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
       />
     )
   }
