fd00da6494f294596409497892ccc6f5bcf332cc
ayan <ayansome1@gmail.com>
2017-12-31 18:06:22 +0530

    jsprettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, {
-  Component
-} from 'react'
-import GoogleLogin from 'react-google-login'
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 
-import './App.css'
+import './App.css';
 
 class App extends Component {
-  render () {
-    const responseGoogle = (response) => {
-      console.log(response)
-    }
+  render() {
+    const responseGoogle = response => {
+      console.log(response);
+    };
     return (
       <GoogleLogin
-        clientId='658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
-        buttonText='Login'
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
       />
-    )
+    );
   }
 }
 
-export default App
+export default App;
